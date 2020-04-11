226e5864c6a48a20c598bdbc4c930793c444cccb
shifty21 <yateenderk@gmail.com>
2020-04-11 10:38:33 +0200

    sharedpreferences setdefault value from string file, oncreate initialization from default value and reset method to reset to default

--- app/src/main/java/com/weather/air_o_inspect/settings/SettingsFragment.java
+++ app/src/main/java/com/weather/air_o_inspect/settings/SettingsFragment.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.SeekBar;
 
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
@@ -21,60 +20,38 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
 
     private SharedPreferences sharedPreferences;
 
-
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.root_preferences);
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         sharedPreferences.registerOnSharedPreferenceChangeListener(this);
 
         SeekBarPreference precipitationSeekBar = findPreference("precipitation_seek");
-        precipitationSeekBar.setValue(0);
-        SeekBarPreference windSeekBar =  findPreference("wind_seek");
-        windSeekBar.setValue(0);
-        SeekBarPreference windGustSeekBar =  findPreference("wind_gust_seek");
-        windGustSeekBar.setValue(0);
-        Preferences.getPreferences();
-        findPreference("wind_seek").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                final int progress = Integer.parseInt(String.valueOf(newValue));
-                preference.setSummary(String.format("Current value: %d", progress));
-                Preferences.getPreferences().setWindSeek(progress);
-                return true;
-            }
-        });
+        Preferences.getPreferences().setPrecipitationSeek(precipitationSeekBar.getValue());
+        SeekBarPreference windSeekBar = findPreference("wind_seek");
+        Preferences.getPreferences().setWindSeek(windSeekBar.getValue());
+        SeekBarPreference windGustSeekBar = findPreference("wind_gust_seek");
+        Preferences.getPreferences().setWindGustSeek(windGustSeekBar.getValue());
 
-
-        findPreference("wind_gust_seek").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                final int progress = Integer.parseInt(String.valueOf(newValue));
-                preference.setSummary(String.format("Current value: %d", progress));
-                Preferences.getPreferences().setWindGustSeek(progress);
-                return true;
-            }
-        });
-
-
-        findPreference("precipitation_seek").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                final int progress = Integer.parseInt(String.valueOf(newValue));
-                preference.setSummary(String.format("Current value: %d", progress));
-                Preferences.getPreferences().setPrecipitationSeek(progress);
-                return true;
-            }
-        });
         Log.i("onCreatePref", String.valueOf(Preferences.getPreferences().getWindSeek()));
         Log.i("onCreatePref", String.valueOf(Preferences.getPreferences().getWindGustSeek()));
         Log.i("onCreatePref", String.valueOf(Preferences.getPreferences().getPrecipitationSeek()));
+
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         view.setBackgroundColor(Color.WHITE);
+    }
+
+    public void resetDefault() {
+        SeekBarPreference windSeekBar = findPreference("wind_seek");
+        windSeekBar.setValue(getResources().getInteger(R.integer.wind_default));
+        SeekBarPreference windGustSeekBar = findPreference("wind_gust_seek");
+        windGustSeekBar.setValue(getResources().getInteger(R.integer.wind_gust_default));
+        SeekBarPreference precipitationSeekBar = findPreference("precipitation_seek");
+        precipitationSeekBar.setValue(getResources().getInteger(R.integer.precipitation_default));
     }
 
     @Override
